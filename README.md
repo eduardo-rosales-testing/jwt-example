1b40d22e7508303ed4e21484c8cebb4a8ea5ca3f
eduardo-rosales-testing <eduardo-rosales-testing123@gmail.com>
2021-05-23 18:38:22 -0700

    updated README, made db directory with article and users.json seed data, and upgraded tests/index.js mocha test file to include the testing of seed data

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
   1. require.main === module
   2. Make file on 111 (running tests with makefile)
   3. Why `mocha tests` passes, but `./node_modules/.bin/mocha tests` fails
+  4. Where does seed data go, when running `sh seed.sh` (running seed data script)
--- tests/index.js
+++ tests/index.js
@@ -3,6 +3,7 @@ const shutdown = require('../app').shutdown;
 const port = require('../app').port;
 const superagent = require('superagent');
 const expect = require('expect.js');
+const seedArticles = require('../db/articles.json');
 
 describe('server', () => {
   before(() => {
@@ -10,11 +11,46 @@ describe('server', () => {
   });
 
   describe('homepage', () => {
-    it('should respond to GET', () => {
+    it('should respond to GET', (done) => {
       superagent.get(`http://localhost:${port}`).end((error, response) => {
         expect(response.status).to.equal(200);
         done();
       });
+    });
+
+    it('should contain posts', (done) => {
+      superagent.get(`http://localhost:${port}`).end((error, res) => {
+        expect(error).to.be(null);
+        expect(res.text).to.be.ok;
+        seedArticles.forEach((item, index, list) => {
+          if (item.published) {
+            expect(res.text).to.contain(`<h2><a href="/articles/${item.slug}">${item.title}`);
+          } else {
+            expect(res.text).not.to.contain(`<h2><a href="/articles/${item.slug}">${item.title}`);
+          }
+        });
+        done();
+      });
+    });
+  });
+
+  describe('article page', () => {
+    it('should display text or 401', (done) => {
+      let n = seedArticles.length;
+      seedArticles.forEach((item, index, list) => {
+        superagent.get(`http://localhost:${port}/articles/${seedArticles[index].slug}`).end((error, res) => {
+          if (item.published) {
+            expect(error).to.be(null);
+            expect(res.status).to.contain(seedArticles[index].text);
+          } else {
+            expect(error).to.be.ok;
+            expect(res.status).to.be(401);
+          }
+          // console.log(item.title)
+          if (index + 1 === n)
+            done();
+        });
+      })
     });
   });
 
